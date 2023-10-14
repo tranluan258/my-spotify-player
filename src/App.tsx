2e43b02e7d1df6b0647ed9089ad7e9ec5ab1e98c
LuanTran <tranluanqqq@gmail.com>
2023-10-14 19:52:59 +0700

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import SpotifyPlayer from "react-spotify-web-playback";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import * as React from 'react';
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 const AUTH_ENDPOINT = import.meta.env.VITE_AUTH_ENDPOINT;
-const RESPONSE_TYPE = "token";
+const RESPONSE_TYPE = 'token';
 const SCOPES = import.meta.env.VITE_SCOPES;
 
 interface IPlaylist {
@@ -23,20 +24,11 @@ interface ITrack {
     name: string;
   };
 }
-
 interface IState {
   isPlaying: boolean;
   track: {
     id: string;
   };
-}
-
-interface IEventTarget extends EventTarget {
-  id: string;
-}
-
-interface IEvent extends React.MouseEvent<HTMLDivElement, MouseEvent> {
-  target: IEventTarget;
 }
 
 function App() {
@@ -48,57 +40,55 @@ function App() {
   const [trackActive, setTrackActive] = useState<string | null>(null);
 
   useEffect(() => {
-    //get token from local storage
-    const _token = localStorage.getItem("token");
+    const _token = localStorage.getItem('token');
     if (_token) {
       setToken(_token);
     }
-  });
+  }, []);
 
   useEffect(() => {
     const hash = window.location.hash
       .substring(1)
-      .split("&")
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .reduce((initial: any, item: string) => {
+      .split('&')
+      .reduce<{ [key: string]: string }>((initial, item) => {
         if (item) {
-          const parts = item.split("=");
+          const parts = item.split('=');
           initial[parts[0]] = decodeURIComponent(parts[1]);
         }
 
         return initial;
       }, {});
 
-    window.location.hash = "";
+    window.location.hash = '';
 
     const _token = hash.access_token;
 
     if (_token) {
       setToken(_token);
-      localStorage.setItem("token", _token);
+      localStorage.setItem('token', _token);
     }
   }, []);
 
   useEffect(() => {
     if (!token) return;
     axios
-      .get("https://api.spotify.com/v1/me/playlists", {
+      .get('https://api.spotify.com/v1/me/playlists', {
         headers: {
-          Authorization: "Bearer " + token,
+          Authorization: 'Bearer ' + token,
         },
       })
       .then((res) => {
         setPlaylist(res.data.items);
-        const currentPlaylist = localStorage.getItem("currentPlaylist");
+        const currentPlaylist = localStorage.getItem('currentPlaylist');
         if (currentPlaylist) {
           setCurrentPlaylist(currentPlaylist);
         } else {
           setCurrentPlaylist(res.data.items[0].id);
-          localStorage.setItem("currentPlaylist", res.data.items[0].id);
+          localStorage.setItem('currentPlaylist', res.data.items[0].id);
         }
       })
       .catch(() => {
-        localStorage.removeItem("token");
+        localStorage.removeItem('token');
         setToken(null);
         window.location.reload();
       });
@@ -107,10 +97,10 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        "https://api.spotify.com/v1/playlists/" + currentPlaylist + "/tracks",
+        'https://api.spotify.com/v1/playlists/' + currentPlaylist + '/tracks',
         {
           headers: {
-            Authorization: "Bearer " + token,
+            Authorization: 'Bearer ' + token,
           },
         }
       )
@@ -120,34 +110,33 @@ function App() {
       });
   }, [currentPlaylist, token]);
 
-  const selectPlaylist = (e: IEvent) => {
-    console.log(e.target);
-    const id: string = e.target.id;
+  const selectPlaylist = (e: React.MouseEvent<HTMLDivElement>) => {
+    const id: string = e.currentTarget.id;
     setCurrentPlaylist(id);
     setPlay(true);
-    localStorage.setItem("currentPlaylist", id);
+    localStorage.setItem('currentPlaylist', id);
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       {!token ? (
-        <div className="Login max-h-full h-56 grid content-center">
-          <button className="">
+        <div className='Login max-h-full h-56 grid content-center'>
+          <button className=''>
             <a
               href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`}
-              className="btn btn--login bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className='btn btn--login bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
             >
               Login to Spotify
             </a>
           </button>
         </div>
       ) : (
-        <div className="flex flex-col justify-center place-items-center">
-          <div className="playlist flex h-[150px]">
+        <div className='flex flex-col justify-center place-items-center'>
+          <div className='playlist flex h-[150px]'>
             {playlist &&
               playlist!.map((item: IPlaylist) => (
                 <div
-                  className="playlist-item px-2  flex  flex-row justify-center place-items-center hover:bg-gray-700"
+                  className='playlist-item px-2  flex  flex-row justify-center place-items-center hover:bg-gray-700'
                   key={item.id}
                   onClick={selectPlaylist}
                   id={item.id}
@@ -156,12 +145,12 @@ function App() {
                     src={item.images[0].url}
                     alt={item.name}
                     id={item.id}
-                    className="h-24 w-24 rounded-full"
+                    className='h-24 w-24 rounded-full'
                   />
-                  <div className="playlist-item-details">
+                  <div className='playlist-item-details'>
                     <h2
                       id={item.id}
-                      className="place-items-center text-white mx-5 border-b-2 border-transparent "
+                      className='place-items-center text-white mx-5 border-b-2 border-transparent '
                     >
                       {item.name}
                     </h2>
@@ -169,29 +158,29 @@ function App() {
                 </div>
               ))}
           </div>
-          <div className="tracks flex flex-col place-items-center justify-center mt-6">
+          <div className='tracks flex flex-col place-items-center justify-center mt-6'>
             {tracks &&
               tracks!.map((item: ITrack) =>
                 trackActive && item.track.id === trackActive ? (
-                  <div className="track-item" key={item.track.id}>
-                    <div className="track-item-details text-red-400">
+                  <div className='track-item' key={item.track.id}>
+                    <div className='track-item-details text-red-400'>
                       <h2>{item.track.name}</h2>
                     </div>
                   </div>
                 ) : (
-                  <div className="track-item" key={item.track.id}>
-                    <div className="track-item-details text-white">
+                  <div className='track-item' key={item.track.id}>
+                    <div className='track-item-details text-white'>
                       <h2>{item.track.name}</h2>
                     </div>
                   </div>
                 )
               )}
           </div>
-          <div className="player w-1/2  mt-6">
+          <div className='player w-1/2  mt-6'>
             {currentPlaylist && (
               <SpotifyPlayer
                 token={token}
-                uris={["spotify:playlist:" + currentPlaylist]}
+                uris={['spotify:playlist:' + currentPlaylist]}
                 play={play}
                 callback={(state: IState) => {
                   if (state.track) setTrackActive(state.track.id);
@@ -200,13 +189,13 @@ function App() {
                   if (state.isPlaying) setPlay(true);
                 }}
                 styles={{
-                  bgColor: "#333",
-                  color: "#fff",
-                  loaderColor: "#fff",
-                  sliderColor: "#1cb954",
-                  savedColor: "#fff",
-                  trackArtistColor: "#ccc",
-                  trackNameColor: "#fff",
+                  bgColor: '#333',
+                  color: '#fff',
+                  loaderColor: '#fff',
+                  sliderColor: '#1cb954',
+                  savedColor: '#fff',
+                  trackArtistColor: '#ccc',
+                  trackNameColor: '#fff',
                 }}
               />
             )}
